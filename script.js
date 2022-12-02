bbfb10131b828c3607ca578345c5e31d7f014ba7
ViktoriiaBusyhina <114439316+ViktoriiaBusyhina@users.noreply.github.com>
2022-12-02 18:19:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,145 @@
+//number 1
+//let d = 7;
+//console.log(d);
+//console.log(d ** 2);
+//console.log(d ** 3)
+
+//number 2
+//let a = Number(prompt("Enter number"))
+//if(a > 0){
+//a = a * 2
+//console.log(a)
+//}
+
+//numer3
+//let b = Number(prompt("Enter number"));
+//if(b > 0){
+//    console.log("положительное");
+//}
+//else if(b < 0){
+ //   console.log("отрицательное");
+//}
+//else if(0 == 0){
+ //    console.log("ноль");
+
+//}
+
+//numer4
+//let c = Number(prompt("Enter number"));
+//let d = Number(prompt("Enter number"));
+//if(c > d){
+// console.log(c +d);
+//}
+//else if(d > c){
+//    console.log(c * d);
+  // }  
+//else if(d == c){
+// console.log("числа одинаковые");
+//}
+
+//numer5
+//let k = Number(prompt("Enter number"));
+//if(k < 0){
+//    k = k ** 2;
+//console.log(2);
+//}
+//else {
+//    console.log("ошибка");
+//}
+
+//numer6
+//let g = Number(prompt("Количество учеников в классе"));
+//let l = Number(prompt("Количество стульев"));
+
+//if(g > l){
+//console.log("Стульев не хватает");
+//}
+//else if(g < l){
+//     console.log("Стульев хватает");
+//}
+//else if(g == l){
+//console.log("Стульев хватает");
+//}
+
+//numer7
+//console.log("Старт")
+//for(let num = 0; num < 10; num++){
+//    console.log(num)
+//}
+//console.log("финиш")
+
+//numer8
+//for(let num = 10; num < 100; num++){
+//    if(num % 5 == 0){
+//console.log(num)}
+//    }
+
+//numer9
+//let m = Number(prompt("Enter number"));
+//let numOne = m + 14;
+//let numTwo = 0
+//for(let i = m; i < numOne; i++){
+//  if(i % 2 != 0  ){
+//numTwo = i;
+//console.log(numTwo);  }   
+//}
+
+//numer10
+//let arr = [10, 50, -15, 15, 26]
+//for( let num = 0; num < arr.length; num++){
+//    if(arr[num] > 15){
+//    console.log(arr[num])}
+//    else if(arr[num] == 15){
+//        console.log(arr[num]) 
+//    }
+//}
+
+//numer11
+//for(let num = 0; num < 100; num++){
+ //   if(num % 2 != 0){
+//        console.log(num)
+ //   }
+//}
+
+//numer12
+//let arr = [10, 50, -15, 15, 26, 18, 6];
+//for( let num = 0; num < arr.length; num++){
+//  if(arr[num] % 3 == 0){
+//console.log(arr[num])}
+ // }
+
+ //numer13
+ //let arr = [1, 6, 3];
+ //let sum = 0;
+ //for( let num = 0; num < arr.length; num++){
+ //   sum += arr[num]}
+ //   console.log(sum)
+
+ //numer14
+ //let sum = 0;
+// for(let num = 0; num < 10; num++){
+//    if(num % 2 != 0)
+//    sum += num
+//   console.log(sum)    
+//}
+
+//numer15
+//let arr = [5, 6, 4, 7, 8, 9];
+//let sum = 0;
+//let sumTwo = 0;
+// for( let num = 0; num < arr.length; num++){
+//    if(arr[num] % 2 == 0)
+//  sum += arr[num]};
+//for( let num = 0; num < arr.length; num++){
+//    if(num % 2 != 0)
+//  sumTwo += arr[num]};
+// let difference = sumTwo - sum;
+// console.log(difference)
+
+
+
+
+
+ 
+
+
